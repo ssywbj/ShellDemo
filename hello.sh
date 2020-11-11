c1fa9fffb67e63b267e4821b970f8f92a3b1dd41
ssywbj <weibj@zhipu-inc.com>
2020-11-12 03:32:16 +0800

    shell变量、压缩指令

--- hello.sh
+++ hello.sh
@@ -2,4 +2,20 @@
 date
 echo "Hello World !"
 mkdir Tmp
-ls -lh ../Android
+#ls -lh ../Android
+
+country="China"
+echo "$country"
+
+for skill in Ada Coffe Action Java; do
+    echo "I am good at ${skill} Script"
+done
+
+zip_name="wbj"
+new_file_name="hello_bak.sh"
+rm ${zip_name}.zip
+cp hello.sh $new_file_name
+zip $zip_name $new_file_name  
+echo "compressing..."
+sleep 0.5
+echo "compress finish"
