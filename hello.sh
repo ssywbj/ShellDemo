89b6d8cc6e2428223d7a6f62d828c6b1fe82475e
weibj <weibj@zhipu-inc.com>
2020-11-13 10:34:51 +0800

    rename variable name

--- hello.sh
+++ hello.sh
@@ -12,10 +12,11 @@ for skill in Ada Coffe Action Java; do
 done
 
 zip_name="wbj"
-new_file_name="hello_bak.sh"
+new_file="hello_bak.sh"
 rm ${zip_name}.zip
-cp hello.sh $new_file_name
-zip $zip_name $new_file_name  
+cp hello.sh $new_file
+zip $zip_name $new_file
 echo "compressing..."
 sleep 0.5
+rm $new_file
 echo "compress finish"
