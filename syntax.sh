8edbaae13adb84161bca560c4ddfe872fc002f9d
weibj <weibj@zhipu-inc.com>
2020-11-30 12:25:05 +0800

    文件测试运算符、字符串运算符

--- syntax.sh
+++ syntax.sh
@@ -263,6 +263,102 @@ else
    echo "返回 false"
 fi
 
+echo "--------- 字符串运算符 -----------"
+:<<!
+= 检测两个字符串是否相等，相等返回true
+!= 检测两个字符串是否相等，不相等返回true
+-z 检测字符串长度是否为0，为0返回true
+-n 检测字符串长度是否不为0，不为0返回true
+$ 检测字符串是否为空，不为空返回true
+!
+a="abc"
+b="efg"
+if [ $a = $b ]
+then
+echo "$a = $b : a 等于 b"
+else
+echo "$a = $b: a 不等于 b"
+fi
+if [ $a != $b ]
+then
+echo "$a != $b : a 不等于 b"
+else
+echo "$a != $b: a 等于 b"
+fi
+if [ -z $a ]
+then
+echo "-z $a : 字符串长度为 0"
+else
+echo "-z $a : 字符串长度不为 0"
+fi
+if [ -n "$a" ]
+then
+echo "-n $a : 字符串长度不为 0"
+else
+echo "-n $a : 字符串长度为 0"
+fi
+if [ $a ]
+then
+echo "$a : 字符串不为空"
+else
+echo "$a : 字符串为空"
+fi
+
+echo "--------- 文件测试运算符 ---------"
+:<<!
+-d file检测文件是否是目录，如果是，则返回true
+-f file检测文件是否是普通文件（既不是目录，也不是设备文件），如果是，则返回true
+-r file检测文件是否可读，如果是，则返回true
+-w file检测文件是否可写，如果是，则返回true
+-x file检测文件是否可执行，如果是，则返回true
+-s file检测文件是否为空（文件大小是否大于0），不为空返回true
+-e file检测文件（包括目录）是否存在，如果是，则返回true
+!
+file="./apply.sh"
+file="./aa"
+file="empty.txt"
+if [ -r $file ]
+then
+echo "文件可读"
+else
+echo "文件不可读"
+fi
+if [ -w $file ]
+then
+echo "文件可写"
+else
+echo "文件不可写"
+fi
+if [ -x $file ]
+then
+echo "文件可执行"
+else
+echo "文件不可执行"
+fi
+if [ -f $file ]
+then
+echo "文件为普通文件"
+else
+echo "文件为特殊文件"
+fi
+if [ -d $file ]
+then
+echo "文件是个目录"
+else
+echo "文件不是个目录"
+fi
+if [ -s $file ]
+then
+echo "文件不为空"
+else
+echo "文件为空"
+fi
+if [ -e $file ]
+then
+echo "文件存在"
+else
+echo "文件不存在"
+fi
 echo -e ---------------- 算术运算 end -----------------"\n"
 
 date
